f7f001df1779d102e50f683c6cd172d145918228
Damiano Giusti <damianogiusti95@gmail.com>
2016-05-13 14:09:55 +0200

    sistemato layout inserimento

--- app/src/main/java/it/tsamstudio/noteme/NuovaNotaFragment.java
+++ app/src/main/java/it/tsamstudio/noteme/NuovaNotaFragment.java
@@ -13,8 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnMenuTabClickListener;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
 
 /**
@@ -23,7 +23,6 @@ import com.roughike.bottombar.OnMenuTabClickListener;
 public class NuovaNotaFragment extends DialogFragment {
 
     private View dialogView;
-    private BottomBar bottomBar;
 
     public NuovaNotaFragment() {
         // Required empty public constructor
@@ -51,6 +50,12 @@ public class NuovaNotaFragment extends DialogFragment {
 
         LayoutInflater inflater = LayoutInflater.from(getContext());
         dialogView = inflater.inflate(R.layout.fragment_nuova_nota, null, false);
+        AHBottomNavigation bottomNavigation = (AHBottomNavigation) dialogView.findViewById(R.id.bottomNavigation);
+
+        for (int i = 0; i < 4; i++) {
+            bottomNavigation.addItem(new AHBottomNavigationItem("" + (i + 1), R.drawable.ic_note_add_white_18dp));
+        }
+
         //bottomBar = setupBottomBar(dialogView, savedInstanceState);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(dialogView);
@@ -60,28 +65,27 @@ public class NuovaNotaFragment extends DialogFragment {
         return dialog;
     }
 
-    private BottomBar setupBottomBar(View view, Bundle savedInstanceState) {
-        BottomBar bottomBar = BottomBar.attach(view, savedInstanceState);
-        bottomBar.setItemsFromMenu(R.menu.bottom_bar_menu, new OnMenuTabClickListener() {
-            @Override
-            public void onMenuTabSelected(int menuItemId) {
-                // TODO switch
-            }
+    /*
+        private BottomBar setupBottomBar(View view, Bundle savedInstanceState) {
+            BottomBar bottomBar = BottomBar.attach(view, savedInstanceState);
+            bottomBar.setItemsFromMenu(R.menu.bottom_bar_menu, new OnMenuTabClickListener() {
+                @Override
+                public void onMenuTabSelected(int menuItemId) {
+                    // TODO switch
+                }
 
-            @Override
-            public void onMenuTabReSelected(int menuItemId) {
+                @Override
+                public void onMenuTabReSelected(int menuItemId) {
 
-            }
-        });
+                }
+            });
 
-        return bottomBar;
-    }
-
+            return bottomBar;
+        }
+    */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (bottomBar != null) {
-            bottomBar.onSaveInstanceState(outState);
-        }
+
     }
 }
