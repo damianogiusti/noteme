68c56d88325f25824e3d594cfddff5ea9c30294b
lucacampara <lcampara96@gmail.com>
2016-05-13 17:55:26 +0200

    creazione nota

--- app/src/main/java/it/tsamstudio/noteme/NuovaNotaFragment.java
+++ app/src/main/java/it/tsamstudio/noteme/NuovaNotaFragment.java
@@ -6,15 +6,22 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.couchbase.lite.CouchbaseLiteException;
+
+import java.io.IOException;
 
 
 /**
@@ -23,6 +30,7 @@ import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 public class NuovaNotaFragment extends DialogFragment {
 
     private View dialogView;
+    private TextView titolo, nota;
 
     public NuovaNotaFragment() {
         // Required empty public constructor
@@ -42,6 +50,12 @@ public class NuovaNotaFragment extends DialogFragment {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        saveNote();
+        Log.d("DETACH", "onDETACH");
+    }
 
     @NonNull
     @Override
@@ -52,9 +66,13 @@ public class NuovaNotaFragment extends DialogFragment {
         dialogView = inflater.inflate(R.layout.fragment_nuova_nota, null, false);
         AHBottomNavigation bottomNavigation = (AHBottomNavigation) dialogView.findViewById(R.id.bottomNavigation);
 
-        for (int i = 0; i < 4; i++) {
-            bottomNavigation.addItem(new AHBottomNavigationItem("" + (i + 1), R.drawable.ic_note_add_white_18dp));
-        }
+        titolo = (TextView)dialogView.findViewById(R.id.etxtTitolo);
+        nota = (TextView)dialogView.findViewById(R.id.etxtNota);
+
+        //for (int i = 0; i < 4; i++) {
+        bottomNavigation.addItem(new AHBottomNavigationItem("" + 0, R.drawable.ic_menu_camera));
+        bottomNavigation.addItem(new AHBottomNavigationItem("" + 1, R.drawable.ic_mic));
+        //}
 
         //bottomBar = setupBottomBar(dialogView, savedInstanceState);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -86,6 +104,31 @@ public class NuovaNotaFragment extends DialogFragment {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+
+    }
+
+    //metodo chiamato quando viene chiuso il dialog
+    private void saveNote(){
+        if (titolo.getText().length() > 0 || nota.getText().length() > 0) {   //se c'è almeno uno dei parametri
+            Nota nota = new Nota();
+            String titoloNota = "Nota senza titolo";
+            if (titolo.getText().length() > 0)
+                titoloNota = "" + titolo.getText();
+            nota.setTitle("" + titoloNota);
+            nota.setText("" + nota.getText());
+            CouchbaseDB db = new CouchbaseDB(getContext());
+            try {
+                db.salvaNota(nota);
+                Log.d("NOTA", "NOTA SALVATA");
+                Log.d("NOTA", "ci sono" + db.leggiNote().size());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (CouchbaseLiteException e) {
+                e.printStackTrace();
+            }
+        } else{
+            Log.d("NOTA", "NOTA NON SALVATA");
+        }
 
     }
 }
