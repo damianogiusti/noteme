eee6baadbbec3f5397c356a631a4b7f70e555f41
Damiano Giusti <damiano.giusti@molo17.com>
2016-05-31 15:31:22 +0200

    scheletro classe S3Manager

---
+++ app/src/main/java/it/tsamstudio/noteme/utils/S3Manager.java
@@ -0,0 +1,53 @@
+package it.tsamstudio.noteme.utils;
+
+import java.util.List;
+
+import it.tsamstudio.noteme.Nota;
+
+/**
+ * Created by damianogiusti on 31/05/16.
+ */
+public class S3Manager {
+
+    private static final String BUCKET_DIR = "tsac-its/noteme";
+
+    private static S3Manager instance;
+
+    public static S3Manager getInstance() {
+        if (instance == null) {
+            instance = new S3Manager();
+        }
+        return instance;
+    }
+
+    /**
+     * Metodo che carica una nota nel server remoto.
+     *
+     * @param nota Nota da caricare
+     */
+    public void uploadNote(Nota nota) {
+        // TODO
+    }
+
+    /**
+     * Metodo che scarica tutte le note dal server remoto.
+     *
+     * @return Lista di note scaricate
+     */
+    public List<Nota> downloadAllNotes() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Metodo che sincronizza tutte le note locali con quelle in remoto, scaricando le note remote
+     * non presenti in locale, e caricando le note locali non presenti in remoto.
+     * Si appoggia al database locale.
+     *
+     * @return List di Nota contenente tutte le note
+     */
+    public List<Nota> sync() {
+        // TODO
+        return null;
+    }
+}
